30404b1ecab53f58e368a29158be3a1026e01c1b
willy <w.lunah@protonmail.com>
2023-05-27 04:22:41 -0400

    agregado navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+export function NavBar() {
+    retunr(
+        <nav>
+            <div>
+                <h3>Nativas</h3>
+            </div>
+            <ul>
+                <li>Informaciones</li>
+                <li>Conocenos</li>
+            </ul>
+            <div>
+                <h3>Iniciar Sesion</h3>
+                <h3>Registrate</h3>
+            </div>
+            iniciar sesion
+        </nav>
+    );
+}

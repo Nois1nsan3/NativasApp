13a4313b6abd7e88c3d3eb886c2a3194540d0412
wonka-luna <w.lunah@protonmail.com>
2023-06-17 16:19:32 -0400

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import { Error404 } from './containers/pages/Error404.jsx'
+// import { Error404 } from './containers/pages/Error404.jsx'
 import { Home } from './containers/pages/Home.jsx'
 import { Login } from './containers/pages/Login.jsx'
 import { Registrate } from './containers/pages/Registrate.jsx'
@@ -18,7 +18,7 @@ function App () {
         <Route path='/registrate' element={<Registrate />} />
         <Route path='/recuperar-contraseña' element={<RecoverPass />} />
         <Route path='/adm' element={<AdmPage />} />
-        <Route path='*' element={<Error404 />} />
+        {/* <Route path='*' element={<Error404 />} /> */}
       </Routes>
     </BrowserRouter>
   )

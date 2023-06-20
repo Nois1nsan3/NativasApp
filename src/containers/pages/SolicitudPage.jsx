1c1bc1e632d1d91428dec47e3544788ed2023c0d
Nois1nsan3 <108773158+Nois1nsan3@users.noreply.github.com>
2023-06-19 21:52:33 -0400

    formulario kk

--- src/containers/pages/SolicitudPage.jsx
+++ src/containers/pages/SolicitudPage.jsx
@@ -1,14 +1,62 @@
+import { useState, useEffect } from 'react'
+import { db } from '../../firebase'
 import { FormularioSolicitud } from '../../components/FormularioSolicitud.jsx'
+import { NavBar } from '../../components/NavBar.jsx'
 
 export function SolicitudPage () {
-  const handleSolicitud = (contacto, explicacion) => {
-    console.log('Solicitud recibida:', contacto, explicacion)
+  const [solicitudes, setSolicitudes] = useState([])
+
+  useEffect(() => {
+    const fetchSolicitudes = async () => {
+      try {
+        const snapshot = await db.collection('solicitudes').get()
+        const solicitudesData = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+        setSolicitudes(solicitudesData)
+      } catch (error) {
+        console.error('Error al obtener las solicitudes:', error)
+      }
+    }
+
+    fetchSolicitudes()
+  }, [])
+
+  const handleSolicitud = async (contacto, explicacion) => {
+    try {
+      const docRef = await db.collection('solicitudes').add({
+        contacto,
+        explicacion,
+        fecha: new Date().toISOString()
+      })
+
+      console.log('Solicitud enviada correctamente. ID:', docRef.id)
+
+      const snapshot = await db.collection('solicitudes').doc(docRef.id).get()
+      const nuevaSolicitud = { id: docRef.id, ...snapshot.data() }
+
+      setSolicitudes((prevSolicitudes) => [nuevaSolicitud, ...prevSolicitudes])
+    } catch (error) {
+      console.error('Error al enviar la solicitud:', error)
+      console.log(
+        'Ocurrió un error al enviar la solicitud. Por favor, inténtalo de nuevo más tarde.'
+      )
+    }
   }
 
   return (
-    <div>
-      <h1>Formulario de Solicitud</h1>
-      <FormularioSolicitud handleSolicitud={handleSolicitud} />
-    </div>
+    <>
+      <NavBar />
+      <div>
+        <FormularioSolicitud handleSolicitud={handleSolicitud} />
+        <ul>
+          {solicitudes.map((solicitud) => (
+            <li key={solicitud.id}>
+              <p>Contacto: {solicitud.contacto}</p>
+              <p>Explicación: {solicitud.explicacion}</p>
+              <p>Fecha: {solicitud.fecha}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   )
 }

0d85550f7a290dc65551a622586c9b6420ca9fe4
willy <w.lunah@protonmail.com>
2023-06-25 00:31:41 -0400

    se vienen cositas uwu

--- src/containers/pages/UserPage.jsx
+++ src/containers/pages/UserPage.jsx
@@ -1,21 +1,24 @@
-import { userAuth } from '../../context/AuthContext'
+// import { userAuth } from '../../context/AuthContext'
+import Menu from '../../components/dashboard/Menu'
+// import Container from '../../components/dashboard/Container'
 
 export function UserPage () {
-  const { logout } = userAuth()
+  // const { logout } = userAuth()
 
-  const handleLogout = () => {
-    logout()
-  }
+  // const handleLogout = () => {
+  //   logout()
+  // }
   return (
-    <div>
-      <h1>User Page</h1>
+    <div className='dash'>
+      <Menu />
+      {/* <Container /> */}
 
-      <button
+      {/* <button
         className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
         onClick={handleLogout}
       >
         Logout
-      </button>
+      </button> */}
     </div>
   )
 }

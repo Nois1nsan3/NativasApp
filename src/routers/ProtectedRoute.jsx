64cd514777c85ef9c16d02b0a2677f7f79a7544a
willy <w.lunah@protonmail.com>
2023-07-15 18:42:49 -0400

    cambios en el commit

--- src/routers/ProtectedRoute.jsx
+++ src/routers/ProtectedRoute.jsx
@@ -1,7 +1,7 @@
 import { userAuth } from '../context/AuthContext'
 import { Navigate } from 'react-router-dom'
 
-export function ProtectorRuta ({ children }) {
+export default function ProtectorRuta ({ children }) {
   const { user } = userAuth()
   if (user === false) {
     return <Navigate to='/' />
